5eb1aa34f16746a347b93c634eee7b746ddcf6b8
David Robison <mrdavidrobison@gmail.com>
2017-07-11 15:46:53 -0600

    add date and print functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-    $("#print_coupon_button").click(function(){
-        window.print();
-    });
+
+  var d = new Date();
+  var surveyDate =  (d.getMonth()+1)+ "/" + d.getDate() + "/" + d.getFullYear();
+  var thirty = 30;
+  var thirtyDate = d.setDate(d.getDate() + thirty); 
+  var expDate =  (d.getMonth()+1)+ "/" + d.getDate() + "/" + d.getFullYear();
+
+  $("#survey_coupon_top_text").html("Date of Survey: " + surveyDate);
+  $("#survey_coupon_bottom_text").html("Expires: " + expDate);
+
+  $("#print_coupon_btn").click(function(){
+    window.open('http://splashcarwashes.com/wp-content/uploads/2017/07/3_off_coupon.png');
+  });
+
 });
