5e6b13c4d4433ec0bce406d9afdd8c174c94fb68
David Robison <mrdavidrobison@gmail.com>
2017-07-12 15:24:41 -0600

    add file

---
+++ footer.php
@@ -0,0 +1,145 @@
+<?php vct_hook_before_footer(); ?>
+<footer>
+<!-- FOOTER START
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div id="footer-background" class="w3-main" style="width:100%;margin:0;padding:0;float:bottom;">
+<!-- Mobile Footer START
+//////////////////////////////////////////////////
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="w3-container w3-hide-large" style="width:100%;margin:0;padding:0;">
+      <div class="w3-row" style="width:100%;margin:0;padding:0;">
+      <div class="w3-container w3-quarter w3-mobile" style="width:100%;margin:0;padding:0;">
+      <ul class="mobile-footer-ul" style="width:100%;margin:0;padding:0;">
+        <li class=""><img class="footer-social-icons" src="http://splashcarwashes.com/wp-content/uploads/2017/03/social-icon-twitter.png"><img class="footer-social-icons" src="http://splashcarwashes.com/wp-content/uploads/2017/03/social-icon-facebook.png"><img class="footer-social-icons" src="http://splashcarwashes.com/wp-content/uploads/2017/03/social-icon-instagram.png"></li>
+        <li class="mobile-footer-2">LET'S CONNECT.</li>
+        <li class="mobile-footer-2">@SPLASHCARWASH</li>
+      </ul>
+      </div>
+      <div class="w3-container w3-quarter w3-mobile" style="width:100%;margin:0;padding:0;">
+      <ul class="mobile-footer-ul" style="width:100%;margin:0;padding:0;">
+        <li class="mobile-footer-1">services</li>       
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=115">Car Wash Services</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=116">Detailing Services</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=117">Oil &amp; Lube Services</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="https://store.splashcarwashes.com/">Shop</a></li>
+      </ul>
+      </div>
+      <div class="w3-container w3-quarter w3-mobile" style="width:100%;margin:0;padding:0;">
+      <ul class="mobile-footer-ul" style="width:100%;margin:0;padding:0;">
+        <li class="mobile-footer-1">initiatives</li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=189">Car Wash Blog</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=208">Awards</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=209">Fundraising</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=211">Community Outreach</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="#">News</a></li>
+      </ul>
+      </div>
+      <div class="mobile-w3-container w3-quarter w3-mobile" style="width:100%;margin:0;padding:0;">
+      <ul class="mobile-footer-ul" style="width:100%;margin:0;padding:0;">
+        <li class="mobile-footer-1">contact</li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=293">About us</a></li>
+        
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=489">Contact us</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=487">Employment</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="http://splashcarwashes.com/?page_id=232">Locations</a></li>
+        <li class="mobile-footer-2"><a class="footer-li-link w3-button w3-hover-blue" style="width:100%" href="#">Subscribe</a></li>
+      </ul>
+      </div>
+    </div>
+  </div>
+<!-- Mibile Footer END
+//////////////////////////////////////////////////
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+
+<!-- Desktop Footer START
+//////////////////////////////////////////////////
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="w3-container w3-hide-medium w3-hide-small" style="width:100%;margin:0;padding:0;">
+    <div class="w3-main" style="width:100%;margin:0;padding:0;">
+      <div class="w3-row" style="width:100%;margin:0;padding:0;">
+      <div class="w3-container w3-quarter" style="width:;margin:0;padding:0;">
+      <ul class="footer-ul">
+        <li><a href="#" class=""><img class="footer-social-icons" src="http://splashcarwashes.com/wp-content/uploads/2017/03/social-icon-twitter.png"></a><a href="#" class=""><img class="footer-social-icons" src="http://splashcarwashes.com/wp-content/uploads/2017/03/social-icon-facebook.png"></a><a href="#" class=""><img class="footer-social-icons" src="http://splashcarwashes.com/wp-content/uploads/2017/03/social-icon-instagram.png"></a></li>
+        <li class="footer-2" style="">LET'S CONNECT.</li>
+        <li class="footer-2" style="">@SPLASHCARWASH</li>
+      </ul>
+      </div>
+      <div class="w3-container w3-quarter" style="width:;margin:0;padding:0;">
+      <ul class="footer-ul">
+        <li class="footer-1">services</li>
+  
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=115">Car Wash Services</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=116">Detailing Services</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=117">Oil &amp; Lube Services</a></li>
+      <li class="footer-2"><a class="footer-li-link" href="https://store.splashcarwashes.com/">Shop</a></li>
+      </ul>
+      </div>
+      <div class="w3-container w3-quarter" style="width:;margin:0;padding:0;">
+      <ul class="footer-ul">
+        <li class="footer-1">initiatives</li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=189">Car Wash Blog</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=208">Awards</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=209">Fundraising</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=211">Community Outreach</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="#">News</a></li>
+      </ul>
+      </div>
+      <div class="w3-container w3-quarter" style="width:;margin:0;padding:0;">
+      <ul class="footer-ul">
+        <li class="footer-1">contact</li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=293">About Us</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=489">Contact us</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=487">Employment</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="http://splashcarwashes.com/?page_id=232">Locations</a></li>
+        <li class="footer-2"><a class="footer-li-link" href="#">Subscribe</a></li>
+      </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Desktop Footer END
+//////////////////////////////////////////////////
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</div>
+<!-- FOOTER END
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</footer>
+<?php vct_hook_after_footer(); ?>
+<?php wp_footer(); ?>
+<?php
+$_links = [
+  'bridgeport'  => 'http://conta.cc/2tNWkEH',
+  'fairfield'   => 'http://conta.cc/2tNWX19',
+  'cheshire' => 'http://conta.cc/2sRbGen',
+  'cos-cob' => 'http://conta.cc/2sR14wq',
+  'cromwell' => 'http://conta.cc/2sQO75A',
+  'darien' => 'http://conta.cc/2sRc5hh',
+  'greenwich' => 'http://conta.cc/2sRcXCf',
+  'hamden' => 'http://conta.cc/2sR3Rpg',
+  'new-haven' => 'http://conta.cc/2sRdRP9',
+  'norwalk' => 'http://conta.cc/2sReJTR',
+  'shelton'=> 'http://conta.cc/2sRde8d',
+  'southbury' => 'http://conta.cc/2sR9DXI',
+  'stamford' => 'http://conta.cc/2sQU0zA',
+  'west-haven' => 'http://conta.cc/2sR1CSK',
+  'wilton' => 'http://conta.cc/2sR4z62',
+  'bedford-hills' => 'http://conta.cc/2cJPxHL',
+  'brewster' => 'http://conta.cc/2tOVt74',
+  'white-plains' => ''
+];
+
+$_url = explode('/', $_SERVER["REQUEST_URI"]);
+if ($_url[2] == 'survey' && $_links[$_url[1]]) {
+  echo('
+    <div id="survey-backdrop">
+    <div id="survey-wrapper">
+      <img id="survey-close" src="/wp-content/uploads/2017/06/close_btn.png" alt="exit button" title="Close Survey">
+      <iframe id="survey-iframe" src="'.$_links[$_url[1]].'" width="800" height="800" seamless></iframe>
+    </div>
+    </div>
+  ');
+}
+?>
+</body>
+</html>
